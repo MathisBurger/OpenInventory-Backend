f9314619bd3988c0689797f7dccf00ebd7f08705
MathisBurger <handymathis@gmail.com>
2021-02-28 17:57:26 +0100

    improved GetAllTablesController.go

--- controller/GetAllTablesController.go
+++ controller/GetAllTablesController.go
@@ -1,23 +1,27 @@
 package controller
 
 import (
-	"encoding/json"
+	"github.com/MathisBurger/OpenInventory-Backend/config"
 	"github.com/MathisBurger/OpenInventory-Backend/database/actions"
 	"github.com/MathisBurger/OpenInventory-Backend/models"
 	"github.com/MathisBurger/OpenInventory-Backend/utils"
 	"github.com/gofiber/fiber/v2"
-	"strconv"
 )
 
+type getAllTablesResponse struct {
+	Message string              `json:"message"`
+	Alert   string              `json:"alert"`
+	Tables  []models.TableModel `json:"tables"`
+}
+
 func GetAllTablesController(c *fiber.Ctx) error {
-	raw := string(c.Body())
-	obj := models.LoginWithTokenRequest{}
-	err := json.Unmarshal([]byte(raw), &obj)
+	obj := new(models.LoginWithTokenRequest)
+	err := c.BodyParser(obj)
 	if err != nil {
-		res, err := models.GetJSONResponse("Invaild JSON body", "alert alert-danger", "error", "None", 200)
-		if err != nil {
-			utils.LogError("[GetAllTablesController.go, 19, InputError] " + err.Error())
+		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
+			utils.LogError(err.Error(), "GetAllTablesController.go", 17)
 		}
+		res, _ := models.GetJSONResponse("Invaild JSON body", "alert alert-danger", "error", "None", 200)
 		return c.Send(res)
 	}
 	if !checkCheckCredsRequestModel(obj) {
@@ -29,18 +33,9 @@ func GetAllTablesController(c *fiber.Ctx) error {
 		return c.Send(res)
 	}
 	tables := actions.GetAllTables(obj.Username, obj.Password, obj.Token)
-	var compiledTables []string
-	for _, table := range tables {
-		cache := "['" + table.Name + "','" + strconv.Itoa(table.Entries) + "','" + table.CreatedAt + "','" + strconv.Itoa(table.MinPermLvl) + "']"
-		compiledTables = append(compiledTables, cache)
-	}
-	msg := ""
-	for _, str := range compiledTables {
-		msg += str + ";"
-	}
-	res, err := models.GetJSONResponse(msg, "alert alert-success", "ok", "None", 200)
-	if err != nil {
-		utils.LogError("[GetAllTablesController.go, 43, ParsingError] " + err.Error())
-	}
-	return c.Send(res)
+	return c.JSON(getAllTablesResponse{
+		"Successfully queried all tables",
+		"alert alert-success",
+		tables,
+	})
 }
