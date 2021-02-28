8f0fb029109ce4617d09e801a15383193ac5632d
MathisBurger <handymathis@gmail.com>
2021-02-28 18:21:21 +0100

    improved ListAllPermGroupsOfTableController.go

--- controller/ListAllPermGroupsOfTableController.go
+++ controller/ListAllPermGroupsOfTableController.go
@@ -1,34 +1,33 @@
 package controller
 
 import (
-	"encoding/json"
+	"github.com/MathisBurger/OpenInventory-Backend/config"
 	"github.com/MathisBurger/OpenInventory-Backend/database/actions"
+	dbModels "github.com/MathisBurger/OpenInventory-Backend/database/models"
 	"github.com/MathisBurger/OpenInventory-Backend/models"
 	"github.com/MathisBurger/OpenInventory-Backend/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
-// request model
-type ListAllPermGroupsOfTableRequest struct {
+type listAllPermGroupsOfTableRequest struct {
 	Username  string `json:"username"`
 	Password  string `json:"password"`
 	Token     string `json:"token"`
 	TableName string `json:"table_name"`
 }
 
-// response model
-type ListAllPermGroupsOfTableResponse struct {
-	PermGroups []models.PermissionModel `json:"perm_groups"`
-	Message    string                   `json:"message"`
+type listAllPermGroupsOfTableResponse struct {
+	PermGroups []dbModels.PermissionModel `json:"perm_groups"`
+	Message    string                     `json:"message"`
 }
 
-// listAllPermGroupsOfTable endpoint
 func ListAllPermGroupsOfTableController(c *fiber.Ctx) error {
-	raw := string(c.Body())
-	obj := ListAllPermGroupsOfTableRequest{}
-	err := json.Unmarshal([]byte(raw), &obj)
+	obj := new(listAllPermGroupsOfTableRequest)
+	err := c.BodyParser(obj)
 	if err != nil {
-		utils.LogError("[ListAllPermGroupsOfTableController.go, 31, InputError] " + err.Error())
+		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
+			utils.LogError(err.Error(), "ListAllPermGroupsOfTableController.go", 29)
+		}
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
@@ -41,59 +40,18 @@ func ListAllPermGroupsOfTableController(c *fiber.Ctx) error {
 		return c.Send(res)
 	}
 	conn := actions.GetConn()
-	stmt, err := conn.Prepare("SELECT `min-perm-lvl` FROM `inv_tables` WHERE `name`=?")
-	if err != nil {
-		utils.LogError("[ListAllPermGroupsOfTableController.go, 46, SQL-StatementError] " + err.Error())
-	}
-	type cacheStruct struct {
-		MinPermlvl int `json:"min-perm-lvl"`
-	}
-	var minPermLvl int
-	resp, err := stmt.Query(obj.TableName)
-	if err != nil {
-		utils.LogError("[ListAllPermGroupsOfTableController.go, 54, SQL-StatementError] " + err.Error())
-	}
-	for resp.Next() {
-		var cache cacheStruct
-		err = resp.Scan(&cache.MinPermlvl)
-		if err != nil {
-			utils.LogError("[ListAllPermGroupsOfTableController.go, 60, SQL-StatementError] " + err.Error())
-		}
-		minPermLvl = cache.MinPermlvl
-	}
-	if !actions.CheckUserHasHigherPermission(conn, obj.Username, minPermLvl, "") {
-		defer resp.Close()
-		defer stmt.Close()
-		defer conn.Close()
+	defer conn.Close()
+	table := actions.GetTableByName(obj.TableName)
+	if !actions.CheckUserHasHigherPermission(conn, obj.Username, table.MinPermLvl, "") {
 		res, _ := models.GetJSONResponse("Your permission is not high enough to view this table", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
-	stmt, err = conn.Prepare("SELECT * FROM `inv_permissions` WHERE `permission-level`>=?")
-	if err != nil {
-		utils.LogError("[ListAllPermGroupsOfTableController.go, 73, SQL-StatementError] " + err.Error())
-	}
-	resp, err = stmt.Query(minPermLvl)
-	if err != nil {
-		utils.LogError("[ListAllPermGroupsOfTableController.go, 77, SQL-StatementError] " + err.Error())
-	}
-	var response []models.PermissionModel
-	for resp.Next() {
-		var cache models.PermissionModel
-		err = resp.Scan(&cache.ID, &cache.Name, &cache.Color, &cache.PermissionLevel)
-		if err != nil {
-			utils.LogError("[ListAllPermsOfUserController.go, 84, SQL-StatementError] " + err.Error())
-		}
-		response = append(response, cache)
-	}
-	defer resp.Close()
-	defer stmt.Close()
-	defer conn.Close()
-	return c.JSON(ListAllPermGroupsOfTableResponse{
-		response,
+	return c.JSON(listAllPermGroupsOfTableResponse{
+		actions.GetAllPermissionsWithHigherPermLvl(table.MinPermLvl),
 		"Successfully fetched all permissiongroups of table",
 	})
 }
 
-func checkListAllPermGroupsOfTableRequest(obj ListAllPermGroupsOfTableRequest) bool {
+func checkListAllPermGroupsOfTableRequest(obj *listAllPermGroupsOfTableRequest) bool {
 	return obj.Username != "" && obj.Password != "" && obj.Token != "" && obj.TableName != ""
 }
--- database/models/PermissionModel.go
+++ database/models/PermissionModel.go
@@ -14,3 +14,13 @@ func (c PermissionModel) Parse(resp *sql.Rows) PermissionModel {
 	_ = resp.Scan(&mdl.ID, &mdl.Name, &mdl.Color, &mdl.PermissionLevel)
 	return mdl
 }
+
+func (c PermissionModel) ParseAll(resp *sql.Rows) []PermissionModel {
+	var answers []PermissionModel
+	for resp.Next() {
+		var mdl PermissionModel
+		_ = resp.Scan(&mdl.ID, &mdl.Name, &mdl.Color, &mdl.PermissionLevel)
+		answers = append(answers, mdl)
+	}
+	return answers
+}
