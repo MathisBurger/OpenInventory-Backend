f4bdd99c6c0071a888d4d0303013ba30098bd72d
MathisBurger <handymathis@gmail.com>
2021-02-28 18:02:34 +0100

    improved GetTableColumnsController.go

--- controller/GetTableColumnsController.go
+++ controller/GetTableColumnsController.go
@@ -1,20 +1,28 @@
 package controller
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/MathisBurger/OpenInventory-Backend/config"
 	"github.com/MathisBurger/OpenInventory-Backend/database/actions"
 	"github.com/MathisBurger/OpenInventory-Backend/models"
 	"github.com/MathisBurger/OpenInventory-Backend/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
+type getTableColumnsRequest struct {
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Token     string `json:"token"`
+	TableName string `json:"table_name"`
+}
+
 func GetTableColumnsController(c *fiber.Ctx) error {
-	raw := string(c.Body())
-	obj := models.GetTableColumnsRequestModel{}
-	err := json.Unmarshal([]byte(raw), &obj)
+	obj := new(getTableColumnsRequest)
+	err := c.BodyParser(obj)
 	if err != nil {
-		utils.LogError("[GetTableColumnsController.go, 17, InputError] " + err.Error())
+		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
+			utils.LogError(err.Error(), "GetTableColumnsController.go", 23)
+		}
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
@@ -22,7 +30,6 @@ func GetTableColumnsController(c *fiber.Ctx) error {
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
-	// login is checked in function below
 	columns := actions.GetTableColumns(obj.Username, obj.Password, obj.Token, obj.TableName)
 	if fmt.Sprintf("%T", columns) == "bool" {
 		res, _ := models.GetJSONResponse("Error while fetching Array", "alert alert-danger", "ok", "None", 200)
@@ -35,6 +42,6 @@ func GetTableColumnsController(c *fiber.Ctx) error {
 	})
 }
 
-func checkGetTableColumnsRequest(obj models.GetTableColumnsRequestModel) bool {
+func checkGetTableColumnsRequest(obj *getTableColumnsRequest) bool {
 	return obj.Username != "" && obj.Password != "" && obj.Token != "" && obj.TableName != ""
 }
--- models/GetTableColumnsRequestModel.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type GetTableColumnsRequestModel struct {
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Token     string `json:"token"`
-	TableName string `json:"table_name"`
-}
