4caebeaf87ea27f45316e49313367e551120b857
MathisBurger <handymathis@gmail.com>
2021-03-01 11:35:26 +0100

    adding comments to DeleteTableController.go

--- controller/DeleteTableController.go
+++ controller/DeleteTableController.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// ---------------------------------------------
+//              deleteTableRequest
+//    This struct contains login credentials
+//                and table-name
+// ---------------------------------------------
 type deleteTableRequest struct {
 	Username  string `json:"username"`
 	Password  string `json:"password"`
@@ -15,38 +20,83 @@ type deleteTableRequest struct {
 	TableName string `json:"table_name"`
 }
 
+/////////////////////////////////////////////////////////////
+//                                                         //
+//                 DeleteTableController                   //
+//            This controller deletes an table             //
+//         It requires deleteTableRequest instance         //
+//                                                         //
+/////////////////////////////////////////////////////////////
 func DeleteTableController(c *fiber.Ctx) error {
+
+	// initializing the request object
 	obj := new(deleteTableRequest)
+
+	// parsing the body into the request object
 	err := c.BodyParser(obj)
+
+	// returns "Wrong JSON syntax" response if error is unequal nil
 	if err != nil {
+
+		// checks if request errors should be logged
 		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
+
+			// log error
 			utils.LogError(err.Error(), "DeleteTableController.go", 23)
 		}
+
+		// returns response
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
+	// check if request has been parsed correctly
 	if !checkDeleteTableRequest(obj) {
+
+		// returns "Wrong JSON syntax" response
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
+	// check login status
 	if actions.MysqlLoginWithToken(obj.Username, obj.Password, obj.Token) {
+
+		// get connection
 		conn := actions.GetConn()
 		defer conn.Close()
+
 		table := actions.GetTableByName(obj.TableName)
+
+		// check if user has higher permission
 		if actions.CheckUserHasHigherPermission(conn, obj.Username, table.MinPermLvl, "") {
+
+			// delete table
 			actions.DropTable(obj.TableName)
+
+			// send response
 			res, _ := models.GetJSONResponse("Successfully deleted table", "alert alert-success", "ok", "None", 200)
 			return c.Send(res)
 		}
+
+		// send invalid permission response
 		res, _ := models.GetJSONResponse("You do not have the permission to perform this", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 
 	}
+
+	// send invalid permission response
 	res, _ := models.GetJSONResponse("You do not have the permission to perform this", "alert alert-danger", "ok", "None", 200)
 	return c.Send(res)
 
 }
 
+/////////////////////////////////////////////////////////////
+//                                                         //
+//                 checkDeleteTableRequest                 //
+//      This function is checking the request object       //
+//        It requires the deleteTableRequest object        //
+//                                                         //
+/////////////////////////////////////////////////////////////
 func checkDeleteTableRequest(obj *deleteTableRequest) bool {
 	return obj.Username != "" && obj.Password != "" && obj.Token != "" && obj.TableName != ""
 }
