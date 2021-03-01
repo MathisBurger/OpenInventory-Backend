c2ae2b0444327397a1dbf1503f9c41c31ddf643a
MathisBurger <handymathis@gmail.com>
2021-03-01 12:55:34 +0100

    adding comments to ListAllPermissionGroupsController.go

--- controller/ListAllPermissionGroupsController.go
+++ controller/ListAllPermissionGroupsController.go
@@ -9,14 +9,28 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// ---------------------------------------------
+//    listAllPermissionGroupsResponse request
+// ---------------------------------------------
 type listAllPermissionGroupsResponse struct {
 	Message          string                     `json:"message"`
 	PermissionGroups []dbModels.PermissionModel `json:"permission_groups"`
 }
 
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//                ListAllPermissionGroupsController               //
+//    This controller changes the minPermLvl of the given table   //
+//        It requires models.LoginWithTokenRequest instance       //
+//                                                                //
+////////////////////////////////////////////////////////////////////
 func ListAllPermissionGroupsController(c *fiber.Ctx) error {
+
+	// init and parse the request object
 	obj := new(models.LoginWithTokenRequest)
 	err := c.BodyParser(obj)
+
+	// check request
 	if err != nil {
 		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
 			utils.LogError(err.Error(), "ListAllPermissionGroupsController.go", 21)
@@ -28,10 +42,13 @@ func ListAllPermissionGroupsController(c *fiber.Ctx) error {
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
+	// check login
 	if !actions.MysqlLoginWithToken(obj.Username, obj.Password, obj.Token) {
 		res, _ := models.GetJSONResponse("You do not have the permission to perform this", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
 	return c.JSON(listAllPermissionGroupsResponse{
 		"Successfully fetched all permission groups",
 		actions.GetAllPermissions(),
