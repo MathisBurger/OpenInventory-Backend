b2544cb4946a93dc2fe17c583e9cfc8a035f5e03
MathisBurger <handymathis@gmail.com>
2021-03-01 12:49:27 +0100

    adding comments to ListAllPermGroupsOfTableController.go

--- controller/ListAllPermGroupsOfTableController.go
+++ controller/ListAllPermGroupsOfTableController.go
@@ -9,6 +9,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// ---------------------------------------------
+//   listAllPermGroupsOfTableRequest request
+// ---------------------------------------------
 type listAllPermGroupsOfTableRequest struct {
 	Username  string `json:"username"`
 	Password  string `json:"password"`
@@ -16,14 +19,28 @@ type listAllPermGroupsOfTableRequest struct {
 	TableName string `json:"table_name"`
 }
 
+// ---------------------------------------------
+//  listAllPermGroupsOfTableResponse response
+// ---------------------------------------------
 type listAllPermGroupsOfTableResponse struct {
 	PermGroups []dbModels.PermissionModel `json:"perm_groups"`
 	Message    string                     `json:"message"`
 }
 
+////////////////////////////////////////////////////////////////////
+//                                                                //
+//               ListAllPermGroupsOfTableController               //
+//    This controller changes the minPermLvl of the given table   //
+//       It requires listAllPermGroupsOfTableRequest instance     //
+//                                                                //
+////////////////////////////////////////////////////////////////////
 func ListAllPermGroupsOfTableController(c *fiber.Ctx) error {
+
+	// init and parse the request object
 	obj := new(listAllPermGroupsOfTableRequest)
 	err := c.BodyParser(obj)
+
+	// check request
 	if err != nil {
 		if cfg, _ := config.ParseConfig(); cfg.ServerCFG.LogRequestErrors {
 			utils.LogError(err.Error(), "ListAllPermGroupsOfTableController.go", 29)
@@ -35,23 +52,32 @@ func ListAllPermGroupsOfTableController(c *fiber.Ctx) error {
 		res, _ := models.GetJSONResponse("Wrong JSON syntax", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
+	// check login
 	if !actions.MysqlLoginWithToken(obj.Username, obj.Password, obj.Token) {
 		res, _ := models.GetJSONResponse("You do not have the permission to perform this", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
 	conn := actions.GetConn()
 	defer conn.Close()
+
 	table := actions.GetTableByName(obj.TableName)
+
+	// check permission
 	if !actions.CheckUserHasHigherPermission(conn, obj.Username, table.MinPermLvl, "") {
 		res, _ := models.GetJSONResponse("Your permission is not high enough to view this table", "alert alert-danger", "ok", "None", 200)
 		return c.Send(res)
 	}
+
 	return c.JSON(listAllPermGroupsOfTableResponse{
 		actions.GetAllPermissionsWithHigherPermLvl(table.MinPermLvl),
 		"Successfully fetched all permissiongroups of table",
 	})
 }
 
+// checks the request
+// struct fields should not be default
 func checkListAllPermGroupsOfTableRequest(obj *listAllPermGroupsOfTableRequest) bool {
 	return obj.Username != "" && obj.Password != "" && obj.Token != "" && obj.TableName != ""
 }
